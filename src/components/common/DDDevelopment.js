070affb0e9c85db1ab2eed631b02fc14ac16a6c6
DPDalev <dpdalev@abv.bg>
2019-04-21 23:25:04 +0300

    Cleaning

--- src/components/common/DDDevelopment.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DDD = () => (
-
-    <img src='http://localhost:3000/../../../public/images/DDDevelopment_Logo.png' alt='Logo' />
-
-)
-
-export default DDD
--- src/javascript/logout.js
+++
@@ -1,9 +0,0 @@
-import requester from './../infrastructure/requester'
-
-const logout = () => {
-    requester.post('user', '_logout', '', '');
-    sessionStorage.clear()
-    console.log('You logged out!')
-}
-export default logout
-
